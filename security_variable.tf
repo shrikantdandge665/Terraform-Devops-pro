1d3ad57a7844fff62cefc8c73f79cff8620a587a
shrikant dandge <shrikantdandge@gmail.com>
2023-09-13 20:56:42 +0530

    added terraform files

---
+++ security_variable.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "test-sg" {
+  name        = "allow_http_ssh"
+  description = "Allow http and inbound traffic"
+  vpc_id      = "vpc-0a4b4aebbb3267b4e"
+
+  ingress {
+    description      = "Http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_ipv4
+    ipv6_cidr_blocks =  var.cidr_ipv6
+  }
+ ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      =  var.cidr_ipv4
+    ipv6_cidr_blocks =  var.cidr_ipv6
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_ipv4
+    ipv6_cidr_blocks = var.cidr_ipv6
+  }
+
+  tags = {
+    Name = "allow_http_ssh"
+  }
+
+}
+
+output "security_group_id" {
+    value = "aws_security_group.test-sg.id"
+    description = "id of security group"
+  }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "cidr_ipv4" {
+  default= ["0.0.0.0/0"]
+}
+
+variable "cidr_ipv6" {
+  default = ["::/0"]
+}
